880d15898401e11b9fb4e5b6537f3cda4791c049
ikaterina <subsall@mail.ru>
2025-03-10 19:09:17 +0300

    hw4 getStringRepresentation()

--- src/org/skypro/skyshop/services/Searchable.java
+++ src/org/skypro/skyshop/services/Searchable.java
@@ -4,6 +4,6 @@ public interface Searchable {
     String getSearchTerm();
     String getContentType();
     default String getStringRepresentation(){
-        return this + " - " + this.getClass();
+        return getSearchTerm() + " - " + getContentType();
     }
 }
